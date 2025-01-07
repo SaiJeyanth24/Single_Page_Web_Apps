1a44f41e3196c637918d895de0731ec99b058b13
Sai Jeyanth <saijeyanth.2000@gmail.com>
2025-01-07 21:42:06 +0530

    add show modal logic

--- Show Modal/script.js
+++ Show Modal/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const openModalBtn=document.querySelectorAll('.show-modal');
+const closeModalBtn=document.querySelector('.close-modal');
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+
+function openModalHandler(){
+    overlay.classList.remove('hidden');
+    modal.classList.remove('hidden');
+}
+
+function closeModalHandler(){
+    overlay.classList.add('hidden')
+    modal.classList.add('hidden');
+}
+
+
+for(let i=0;i<openModalBtn.length;i++)
+    openModalBtn[i].addEventListener('click',openModalHandler);
+
+closeModalBtn.addEventListener('click',closeModalHandler)
+
+overlay.addEventListener('click',closeModalHandler);
+
